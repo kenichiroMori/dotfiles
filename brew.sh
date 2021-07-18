488e01f87d45b6be2257aab477d2ce77128ed8da
Kenichiro Mori <k_mori@cloudpack.jp>
2021-07-18 20:00:57 +0900

    feat: brew.sh修正

--- brew.sh
+++ brew.sh
@@ -3,7 +3,6 @@ set -Ceuo pipefail
 
 echo "installing Homebrew ..."
 which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-cd $THIS_DIR
 
 echo "run brew doctor ..."
 which brew >/dev/null 2>&1 && brew doctor
